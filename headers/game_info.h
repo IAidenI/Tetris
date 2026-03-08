eb9c6c22a6d1e62632e993199cbe52615ed1e0aa
Nathan Bole-Richard <133039461+IAidenI@users.noreply.github.com>
2026-03-08 10:59:18 +0100

    Update version and version date in game_info.h

--- headers/game_info.h
+++ headers/game_info.h
@@ -1,8 +1,8 @@
 #ifndef INFO_H
 #define INFO_H
 
-#define VERSION      "V 0.4"
-#define VERSION_DATE "06/04/2025"
+#define VERSION      "V 1.0"
+#define VERSION_DATE "08/03/2026"
 #define AUTHOR       "Aiden"
 #define GAME         "Tetris"
 
