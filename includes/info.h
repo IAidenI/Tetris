a69394ba16fd5e82b87f4cb45c23d45b41fc4346
IAidenI <bolerichardnathan@gmail.com>
2026-03-18 13:40:56 +0100

    feat: add UI menu and display commands

--- includes/info.h
+++ includes/info.h
@@ -1,7 +1,7 @@
 #ifndef INFO_H
 #define INFO_H
 
-#define VERSION      "V 1.2.7"
+#define VERSION      "V 1.2.8"
 #define VERSION_DATE "15/03/2026"
 #define AUTHOR       "Aiden"
 #define GAME         "Tetris"
